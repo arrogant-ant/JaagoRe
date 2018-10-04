519a8bbaadd46f0af33285f48b944fa6544ed1cb
Sud <sudhanshuambastha@gmail.com>
2018-10-05 01:21:21 +0530

    suggest alarm

---
+++ app/src/main/java/iris/example/sabita_sant/alarm/controller/SuggestAlarmHelper.java
@@ -0,0 +1,8 @@
+package iris.example.sabita_sant.alarm.controller;
+
+/**
+ * Created by Sud on 10/4/18.
+ */
+
+public class SuggestAlarmHelper {
+}
---
+++ app/src/main/java/iris/example/sabita_sant/alarm/controller/SuggestAlarmWorker.java
@@ -0,0 +1,30 @@
+package iris.example.sabita_sant.alarm.controller;
+
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import androidx.work.Worker;
+import iris.example.sabita_sant.alarm.backend.Alarm;
+import iris.example.sabita_sant.alarm.backend.AlarmDatabase;
+import iris.example.sabita_sant.alarm.utils.AlarmNotification;
+
+/**
+ * Created by Sud on 10/4/18.
+ */
+
+public class SetAlarmWorker extends Worker {
+    @NonNull
+    @Override
+    public Result doWork() {
+        AlarmDatabase db = AlarmDatabase.getInstance(getApplicationContext());
+        List<Alarm> alarms = db.alarmDao().getAll();
+        for (Alarm alarm:
+             alarms) {
+            if(alarm.isActive())
+                return Result.SUCCESS;
+        }
+        new AlarmNotification(getApplicationContext()).suggestAlarmNotification();
+        return Result.SUCCESS;
+    }
+}
